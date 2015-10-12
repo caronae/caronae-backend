c8c6000f2c23c4db40c2c506a4856feb5d897cf8
Leonardo Marques <leommarques7@gmail.com>
2015-10-12 10:24:55 -0300

    rota de cadastro com nome e token de novo

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,6 +33,21 @@ Route::get('signup/{name}', function($name) {
 	return $name . ' cadastrado';
 });
 
+Route::get('signup/{name}/{token}', function($name, $token) {
+	if (User::where('name', $name)->count() > 0) {
+		return $name . ' já existe';
+	}
+	
+    $user = new User();
+
+    $user->name = $name;
+    $user->token = $token;
+
+    $user->save();
+	
+	return $name . ' cadastrado com o token ' . $token;
+});
+
 //rota padrão do laravel podemos excluir depois
 Route::get('/', function () {
     return View::make('welcome');
