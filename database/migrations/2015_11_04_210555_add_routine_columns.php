ef4653fc06c1ba5754aeda0c21b1ce1d4a89e35e
Mario Cecchi <macecchi@gmail.com>
2015-11-04 23:44:06 -0200

    Criação de rotinas das caronas

---
+++ database/migrations/2015_11_04_210555_add_routine_columns.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRoutineColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rides', function (Blueprint $table) {
+            $table->string('week_days')->nullable();
+            $table->date('repeats_until')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
