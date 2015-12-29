5853f6a8257b3c027bb136c5c491f9bedb7b9402
Leonardo Marques <leommarques7@gmail.com>
2015-12-29 17:42:06 -0200

    faltou incluir o "use SoftDeletes"

--- app/Ride.php
+++ app/Ride.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ride extends Model
 {
--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Model 
 {
