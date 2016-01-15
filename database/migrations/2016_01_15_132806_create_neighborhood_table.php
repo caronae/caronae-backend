064878ea6cf66ca39f1fc4f7885adc785780f97e
daniellima <daniellima.pessoal@gmail.com>
2016-01-15 15:11:38 -0200

    Adicionando migration para a tabela de bairros

---
+++ database/migrations/2016_01_15_132806_create_neighborhood_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNeighborhoodTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('neighborhoods', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('zone');
+            $table->decimal('distance');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('neighborhoods');
+    }
+}
