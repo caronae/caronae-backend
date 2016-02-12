f839a1aac6e0bc7427b767e8bc89b4299ad5250d
daniellima <daniellima.pessoal@gmail.com>
2016-02-12 21:19:12 -0200

    Refactor
     - Retirando o arquivo helpers.php que não está sendo usado.

--- app/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if(!function_exists('trimText')){
-    function trimText($text, $howMuch) {
-        return strlen($text) > $howMuch ? substr($text, 0, $howMuch - 3) . '...' : $text;
-    }
-}
