dde87d7bba65b3a5a19a108b4715e837da556de2
Leonardo Marques <leommarques7@gmail.com>
2016-03-18 18:30:56 -0300

    migration para fazer o token ser unique

---
+++ database/migrations/2016_03_18_182935_token_unique.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TokenUnique extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unique('token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropUnique('users_token_unique');
+        });
+    }
+}
