93ef8faee2381e31bfdd26309960b221bc56904a
Leonardo Marques <leommarques7@gmail.com>
2016-03-25 16:19:55 -0300

    novas paginas de sobre e termos de uso

--- resources/views/static_pages/sobre.blade.php
+++ resources/views/static_pages/sobre.blade.php
@@ -1,29 +1,11 @@
-<html>
 <head>
   <meta charset="utf-8">
   <title>Sobre o Caronaê</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="generator" content="Webflow">
-  <link rel="stylesheet" type="text/css" href="{{ asset("css/normalize.css") }}">
-  <link rel="stylesheet" type="text/css" href="{{ asset("css/webflow.css") }}">
-  <link rel="stylesheet" type="text/css" href="{{ asset("css/caronae.webflow.css") }}">
-  <script src="{{ asset("js/webfont_1.4.7.js") }}"></script>
-  <script>
-    WebFont.load({
-      google: {
-        families: ["Montserrat:400,700","Varela:400"]
-      }
-    });
-  </script>
-  <script type="text/javascript" src="{{ asset("js/modernizr.js") }}"></script>
-  <style>
-    .pergunta-title{
-        cursor:pointer;
-    }
-  </style>
+  <link rel="stylesheet" type="text/css" href="{{ asset("css/normalize.css") }}>
+  <link rel="stylesheet" type="text/css" href="{{ asset("css/webflow.css") }}>
+  <link rel="stylesheet" type="text/css" href="{{ asset("css/caronae.webflow.css") }}>
 </head>
 <body class="landing-page">
-  <div data-collapse="medium" data-animation="over-right" data-duration="400" data-contain="1" class="w-nav header">
     <div class="w-row lines-container">
       <div class="w-col w-col-2 w-col-small-2 w-col-tiny-2 header-line brown"></div>
       <div class="w-col w-col-2 w-col-small-2 w-col-tiny-2 header-line blue"></div>
@@ -33,13 +15,18 @@
       <div class="w-col w-col-2 w-col-small-2 w-col-tiny-2 header-line red"></div>
     </div>
     <div class="w-container">
-      <a href="#top" class="w-nav-brand logo"><img src="{{ asset("images/logo_caronae_with_text.png") }}">
+      <a href="#top" class="w-nav-brand logo"><img src="{{ asset("images/caronae-logo.png") }}">
       </a>
     </div>
     <h1 align="center">Sobre o Caronaê</h1>
-    <div class="text">Criado por alunos da UFRJ, o Caronaê é um aplicativo exclusivo à comunidade UFRJ, com pontos de carona que facilitam o encontro entre motoristas e caroneiros. O aplicativo tem como objetivo facilitar o acesso dos alunos à Cidade Universitária, catalisando os motoristas e demais tripulantes em um serviço digital simples e eficiente. Os dez pontos de carona estão espalhados pelo campus, posicionados em locais de visibilidade, próximos aos acessos dos estacionamentos. 
+    <div class="text">
+	Aplicativo Android desenvolvido por:<br>
+	<a href="http://www.github.com/leomarques">Leonardo Miranda Marques</a><br><br>
+	Aplicativo iOS desenvolvido por:<br>
+	<a href="http://meriw.xyz">Mário Alberto Cecchi</a><br>
+	<br><br>
+	Criado por alunos da UFRJ, o Caronaê é um aplicativo exclusivo à comunidade UFRJ, com pontos de carona que facilitam o encontro entre motoristas e caroneiros. O aplicativo tem como objetivo facilitar o acesso dos alunos à Cidade Universitária, catalisando os motoristas e demais tripulantes em um serviço digital simples e eficiente. Os dez pontos de carona estão espalhados pelo campus, posicionados em locais de visibilidade, próximos aos acessos dos estacionamentos. 
       <br><br>O acesso ao Caronaê é integrado aos bancos de dados da UFRJ, o que garante segurança e confiabilidade aos usuários - somente alunos, professores e demais funcionários podem utilizar o aplicativo. Depois de receber a liberação do acesso, o usuário checa as caronas disponíveis e decide a sua viagem ideal de acordo com o ponto de partida ou de destino.
     </div>
-  </div>
 </body>
 </html>
