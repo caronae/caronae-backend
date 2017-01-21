62d6da5c8d2aac19a6289ef3e6190fd542a0669b
Mario Cecchi <macecchi@gmail.com>
2017-01-20 22:12:57 -0200

    Corrige migration removendo usuários duplicados/inválidos

--- database/migrations/2016_02_26_143506_id_ufrj_unique.php
+++ database/migrations/2016_02_26_143506_id_ufrj_unique.php
@@ -12,6 +12,9 @@ class IdUfrjUnique extends Migration
      */
     public function up()
     {
+        // Fix to delete users with an empty UFRJ id
+        $this->clearInvalidUsers();
+
 		Schema::table('users', function (Blueprint $table) {
             $table->unique('id_ufrj');
         });
@@ -27,5 +30,34 @@ class IdUfrjUnique extends Migration
         Schema::table('users', function ($table) {
             $table->dropUnique('users_id_ufrj_unique');
         });
+    }
+
+    private function clearInvalidUsers()
+    {
+        $this->clearUsersWithoutUFRJId();
+        $this->mergeUserWithDuplicatedIds();
+    }
+
+    private function clearUsersWithoutUFRJId()
+    {        
+        // Delete relationships
+        DB::connection()->getPdo()->exec("DELETE 
+            FROM ride_user ru
+            USING users u
+            WHERE ru.user_id = u.id
+            AND u.id_ufrj = ''
+        ");
+
+        // Delete the users
+        DB::connection()->getPdo()->exec("DELETE FROM users WHERE id_ufrj = ''");
+    }
+
+    private function mergeUserWithDuplicatedIds()
+    {
+        // Move all relationships to one user
+        DB::connection()->getPdo()->exec("UPDATE ride_user SET user_id = 8540 WHERE user_id IN (8539, 8541)");
+
+        // Delete the duplicated users
+        DB::connection()->getPdo()->exec("DELETE FROM users WHERE id IN (8539, 8541)");
     }
 }
