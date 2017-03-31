a91b59ac33e01a341b1f7fe475df209fb893e98b
Mario Cecchi <macecchi@gmail.com>
2017-03-31 01:52:41 -0300

    Adiciona token automaticamente no cadastro

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,8 +31,12 @@ class UserController extends Controller
 
     public function store(SignUpRequest $request)
     {
-        $user = User::create($request->all())->fresh();
-        return response()->json($user, 201);
+        $user = new User;
+        $user->fill($request->all());
+        $user->token = strtoupper(substr(base_convert(sha1(uniqid() . rand()), 16, 36), 0, 6));
+        $user->save();
+
+        return response()->json($user->fresh(), 201);
     }
 
     public function signUpIntranet($idUFRJ, $token, SigaService $siga)
