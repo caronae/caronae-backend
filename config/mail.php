824d6274c23adcfab738046f6cc6473979947bee
Mario Cecchi <mcecchi@thoughtworks.com>
2017-06-19 00:18:14 -0300

    Adiciona configuração padrão pra envio de emails

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'caronae@fundoverde.ufrj.br'),
+        'name' => env('MAIL_FROM_NAME', 'Caronaê'),
     ],
 
     /*
