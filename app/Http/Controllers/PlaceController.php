d30939372810f0aad5228ea49ddc9406a86eb458
Mario Cecchi <mcecchi@thoughtworks.com>
2017-07-04 00:49:33 -0300

    Cache zonas e centros

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -4,9 +4,13 @@ namespace Caronae\Http\Controllers;
 
 use Caronae\Models\Hub;
 use Caronae\Models\Neighborhood;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class PlaceController extends Controller
 {
+    const CACHE_TIME_MINUTES = 60*24*365;
+
 	public function index()
 	{
 		return [
@@ -15,26 +19,32 @@ class PlaceController extends Controller
 		];
 	}
 
-	private function getZones()
+	public function getZones()
     {
-        $zones = Neighborhood::select('zone')->distinct()->pluck('zone');
-        return $zones->map(function($zone) {
-            return [
-                'name' => $zone,
-                'neighborhoods' => Neighborhood::where('zone', $zone)->pluck('name')
-            ];
+        return Cache::remember('zones', self::CACHE_TIME_MINUTES, function () {
+            Log::info('Loading zones from database');
+            $zones = Neighborhood::select('zone')->distinct()->pluck('zone');
+            return $zones->map(function($zone) {
+                return [
+                    'name' => $zone,
+                    'neighborhoods' => Neighborhood::where('zone', $zone)->pluck('name')
+                ];
+            });
         });
     }
 
     private function getCenters()
     {
-        $centers = Hub::select('center', 'campus')->distinct()->get();
-        return $centers->map(function($center) {
-            return [
-                'name' => $center->center,
-                'campus' => $center->campus,
-                'hubs' => Hub::where('center', $center->center)->pluck('name')
-            ];
+        return Cache::remember('centers', self::CACHE_TIME_MINUTES, function () {
+            Log::info('Loading centers from database');
+            $centers = Hub::select('center', 'campus')->distinct()->get();
+            return $centers->map(function ($center) {
+                return [
+                    'name' => $center->center,
+                    'campus' => $center->campus,
+                    'hubs' => Hub::where('center', $center->center)->pluck('name')
+                ];
+            });
         });
     }
 }
