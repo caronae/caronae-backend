eec90042435989ee3d7bf4f4dffbbe6a4bf1e041
Mario Cecchi <mcecchi@thoughtworks.com>
2017-07-06 22:41:55 -0300

    Adiciona tracking do Analytics no admin

--- resources/views/templates/login.blade.php
+++ resources/views/templates/login.blade.php
@@ -13,6 +13,16 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-97867968-4', 'auto');
+        ga('send', 'pageview');
+    </script>
 </head>
 
 <body>
