3f100396c2631dad76f0fe7cfca80cb7bd3317a3
Mario Cecchi <macecchi@gmail.com>
2018-02-25 23:43:59 -0300

    Autoriza requisições na API autenticadas com JWT

--- app/Http/Middleware/ApiV1Authenticate.php
+++ app/Http/Middleware/ApiV1Authenticate.php
@@ -4,22 +4,24 @@ namespace Caronae\Http\Middleware;
 
 use Caronae\Models\User;
 use Closure;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class ApiV1Authenticate
 {
     public function handle($request, Closure $next)
     {
-        if (empty($request->header('token')) || ($user = User::where('token', $request->header('token'))->first()) == NULL || $user->banned) {
-            return response()->json(['error' => 'User token not authorized.'], 401);
+        if (!empty($request->header('token'))) {
+            return $this->handleLegacyTokenAuthentication($request, $next);
         }
 
-        $request->merge([
-            'currentUser' => $user
-        ]);
+        if (!empty($request->header('Authorization'))) {
+            return $this->handleJWTAuthentication($request, $next);
+        }
 
-        $this->updateUserAppInfo($request);
-
-        return $next($request);
+        return response()->json(['error' => 'Authentication required.'], 401);
     }
 
     private function updateUserAppInfo($request)
@@ -36,6 +38,46 @@ class ApiV1Authenticate
                 $request->currentUser->app_version = $version;
                 $request->currentUser->save();
             }
+        }
+    }
+
+    private function handleLegacyTokenAuthentication($request, Closure $next)
+    {
+        if (($user = User::where('token', $request->header('token'))->first()) == NULL || $user->banned) {
+            return response()->json(['error' => 'User token not authorized.'], 401);
+        }
+
+        $request->merge([
+            'currentUser' => $user
+        ]);
+
+        auth()->setUser($user);
+
+        $this->updateUserAppInfo($request);
+
+        return $next($request);
+    }
+
+    private function handleJWTAuthentication($request, Closure $next)
+    {
+        try {
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+
+            $request->merge([
+                'currentUser' => $user
+            ]);
+
+            $this->updateUserAppInfo($request);
+            return $next($request);
+
+        } catch (TokenExpiredException $e) {
+            return response()->json(['token_expired'], $e->getStatusCode());
+        } catch (TokenInvalidException $e) {
+            return response()->json(['token_invalid', JWTAuth::parseToken()], $e->getStatusCode());
+        } catch (JWTException $e) {
+            return response()->json(['token_absent'], $e->getStatusCode());
         }
     }
 }
