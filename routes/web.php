b6768d3969e14834f0054bf7d889debdfcdaff80
Mario Cecchi <macecchi@gmail.com>
2018-03-31 13:03:42 -0300

    Remove redirects pra páginas estáticas

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,3 @@ Route::get('/', function() {
 });
 
 Route::get('carona/{id}', 'RideController@show');
-
-// Static pages
-/** @deprecated */
-
-Route::get('static_pages/sobre.html', function(){
-    return redirect()->away('https://caronae.org/sobre_mobile.html');
-});
-
-Route::get('static_pages/termos.html', function(){
-    return redirect()->away('https://caronae.org/termos_mobile.html');
-});
-
-Route::get('static_pages/faq.html', function(){
-    return redirect()->away('https://caronae.org/faq.html?mobile');
-});
