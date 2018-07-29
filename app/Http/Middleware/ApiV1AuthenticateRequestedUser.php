efa630e89a6a36814a4c765913bbb1274eb6d47e
Mario Cecchi <macecchi@gmail.com>
2018-07-29 18:34:52 -0300

    Adiciona log quando usuário tenta acessar dados de outro usuário

--- app/Http/Middleware/ApiV1AuthenticateRequestedUser.php
+++ app/Http/Middleware/ApiV1AuthenticateRequestedUser.php
@@ -4,12 +4,19 @@ namespace Caronae\Http\Middleware;
 
 use Auth;
 use Closure;
+use Log;
 
 class ApiV1AuthenticateRequestedUser extends ApiV1Authenticate
 {
     public function handle($request, Closure $next)
     {
-        if ($request->user != Auth::user()) {
+        $user = Auth::user();
+        if ($request->user != $user) {
+            Log::info('Request denied with a 403 because the requested user does not match the authenticated user.', [
+                'path' => $request->path(),
+                'requested_user' => $request->user->id,
+                'authenticated_user' => $user->id,
+            ]);
             return response()->json(['error' => 'You are not authorized.'], 403);
         }
 
