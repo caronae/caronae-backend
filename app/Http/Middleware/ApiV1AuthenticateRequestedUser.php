b83d0d9d8217b5e858484b87b346a55e8305a056
Mario Cecchi <macecchi@gmail.com>
2018-07-29 20:31:00 -0300

    Corrige verificação do usuário para requests da API

--- app/Http/Middleware/ApiV1AuthenticateRequestedUser.php
+++ app/Http/Middleware/ApiV1AuthenticateRequestedUser.php
@@ -10,12 +10,12 @@ class ApiV1AuthenticateRequestedUser extends ApiV1Authenticate
 {
     public function handle($request, Closure $next)
     {
-        $user = Auth::user();
-        if ($request->user != $user) {
+        $authenticatedUserID = Auth::id();
+        if ($request->user->id != $authenticatedUserID) {
             Log::info('Request denied with a 403 because the requested user does not match the authenticated user.', [
                 'path' => $request->path(),
                 'requested_user' => $request->user->id,
-                'authenticated_user' => $user->id,
+                'authenticated_user' => $authenticatedUserID,
             ]);
             return response()->json(['error' => 'You are not authorized.'], 403);
         }
