65a5cc545521a824099c9679a613126bbeeb009c
Mario Cecchi <macecchi@gmail.com>
2018-09-23 20:54:24 -0300

    Cria migração para criptografar mensagens existentes no banco

---
+++ database/migrations/2018_09_23_202653_encrypt_messages.php
@@ -0,0 +1,24 @@
+<?php
+
+use Caronae\Models\Message;
+use Illuminate\Database\Migrations\Migration;
+
+class EncryptMessages extends Migration
+{
+    public function up()
+    {
+        Message::all()->each(function ($message) {
+           $message->body = $message->getAttributeValue('body');
+           $message->save();
+        });
+    }
+
+    public function down()
+    {
+        Message::all()->each(function ($message) {
+            DB::connection()->getPdo()->exec("
+                UPDATE messages SET body = '" . $message->body . "' WHERE id = " . $message->id . "
+            ");
+        });
+    }
+}
