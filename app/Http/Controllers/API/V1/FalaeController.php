6920e8fe4c22fa8ec000f08bf45ead4bbd1525c2
Mario Cecchi <macecchi@gmail.com>
2018-09-28 14:05:19 -0300

    Remove header From do envio de email

--- app/Http/Controllers/API/V1/FalaeController.php
+++ app/Http/Controllers/API/V1/FalaeController.php
@@ -4,6 +4,7 @@ namespace Caronae\Http\Controllers\API\v1;
 
 use Caronae\Http\Controllers\BaseController;
 use Illuminate\Http\Request;
+use Log;
 use Mail;
 
 class FalaeController extends BaseController
@@ -19,9 +20,10 @@ class FalaeController extends BaseController
         $subject = $request->subject;
         $body = $request->message . "\nID UFRJ: " . $user->id_ufrj;
 
+        Log::info('Enviando mensagem através do Falaê', ['user_id' => $user->id, 'user_email' => $user->email, 'subject' => $subject, 'message' => $body]);
+
         Mail::raw($body, function ($message) use ($user, $subject) {
             $message->to('caronae@fundoverde.ufrj.br');
-            $message->from($user->email, $user->name);
             $message->replyTo($user->email, $user->name);
             $message->subject($subject);
         });
