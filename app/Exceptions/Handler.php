8803db0d328b25daf9f09f77efef00843d055208
Mario Cecchi <macecchi@gmail.com>
2018-10-02 15:16:32 -0300

    Remove handler de erro personalizado

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,50 +2,17 @@
 
 namespace Caronae\Exceptions;
 
-use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
     protected $dontReport = [
-        \Illuminate\Auth\AuthenticationException::class,
-        \Illuminate\Auth\Access\AuthorizationException::class,
-        \Symfony\Component\HttpKernel\Exception\HttpException::class,
-        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
-        \Illuminate\Session\TokenMismatchException::class,
-        \Illuminate\Validation\ValidationException::class,
+        //
     ];
 
     protected $dontFlash = [
         'password',
         'password_confirmation',
     ];
-
-    public function render($request, Exception $e)
-    {
-        if ($request->expectsJson() && !($e instanceof ValidationException)) {
-            if ($e instanceof ModelNotFoundException) {
-                return response()->json(['error' => 'Not found'], 404);
-            }
-
-            $response = [
-                'error' => 'Sorry, something went wrong.'
-            ];
-
-            if (config('app.debug')) {
-                $response['exception'] = get_class($e);
-                $response['message'] = $e->getMessage();
-                $response['trace'] = $e->getTrace();
-            }
-
-            $status = $this->isHttpException($e) ? $e->getStatusCode() : 500;
-
-            return response()->json($response, $status);
-        }
-
-        return parent::render($request, $e);
-    }
 
 }
