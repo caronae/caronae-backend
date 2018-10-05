cb88edfd5f60554e7b17a78a964c4c616a0cf04e
Mario Cecchi <macecchi@gmail.com>
2018-10-05 19:00:44 -0300

    Cria script pra rodar os testes e pega exit code correto

---
+++ scripts/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
+SRC_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+docker-compose -f ${SRC_DIR}/../docker/docker-compose.test.yml up --build --exit-code-from caronae-backend-tests
