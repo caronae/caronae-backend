7ad5642979db21fe057f01fb306b09c6848b85e4
Mario Cecchi <macecchi@gmail.com>
2018-10-05 19:06:52 -0300

    Remove variável de BASH_SOURCE do test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 set -euo pipefail
 
-SRC_DIR=$(dirname "${BASH_SOURCE[0]}")
-
-docker-compose -f ${SRC_DIR}/../docker/docker-compose.test.yml up --build --exit-code-from caronae-backend-tests
+docker-compose -f docker/docker-compose.test.yml up --build --exit-code-from caronae-backend-tests
