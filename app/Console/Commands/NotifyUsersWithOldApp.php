a93b999ea03173f6ed74808e2de0a7222836f743
Mario Cecchi <macecchi@gmail.com>
2018-10-19 17:38:17 -0300

    Não manda notificação de update pra usuários que já foram notificados nos últimos 15 dias

--- app/Console/Commands/NotifyUsersWithOldApp.php
+++ app/Console/Commands/NotifyUsersWithOldApp.php
@@ -2,6 +2,8 @@
 
 namespace Caronae\Console\Commands;
 
+use Carbon\Carbon;
+use Caronae\Models\User;
 use Caronae\Notifications\UpdateAppNotification;
 use Caronae\Services\UserAppService;
 use DB;
@@ -11,7 +13,7 @@ use Log;
 class NotifyUsersWithOldApp extends Command
 {
     protected $signature = 'user:notify-old-app';
-    protected $description = 'Notify users that are using an old version to upgrade';
+    protected $description = 'Notify users that are using an old version to update';
 
     /**
      * @var UserAppService
@@ -27,9 +29,20 @@ class NotifyUsersWithOldApp extends Command
     public function handle()
     {
         $users = $this->appService->getActiveUsersWithOldAppVersions();
+        $users = $this->excludeUsersThatWereAlreadyNotified($users);
 
         Log::info("Enviando aviso de app desatualizado para {$users->count()} usuários");
         $users->each->notify(new UpdateAppNotification());
+    }
+
+    private function excludeUsersThatWereAlreadyNotified($users)
+    {
+        return $users->reject(function (User $user) {
+            return $user->notifications()
+                ->where('type', UpdateAppNotification::class)
+                ->whereDate('created_at', '>', new Carbon('15 days ago'))
+                ->exists();
+        });
     }
 
 }
--- tests/commands/NotifyUsersWithOldAppTest.php
+++ tests/commands/NotifyUsersWithOldAppTest.php
@@ -3,11 +3,13 @@
 namespace Caronae\Console\Commands;
 
 
+use Carbon\Carbon;
 use Caronae\Models\User;
 use Caronae\Notifications\UpdateAppNotification;
 use Caronae\Services\UserAppService;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Support\Facades\Notification;
 use Mockery;
 use Tests\TestCase;
 
@@ -15,18 +17,56 @@ class NotifyUsersWithOldAppTest extends TestCase
 {
     use DatabaseTransactions;
 
+    private $mock;
+    private $command;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        Notification::fake();
+        $this->mock = Mockery::mock(UserAppService::class);
+        $this->command = new NotifyUsersWithOldApp($this->mock);
+    }
+
     /** @test */
     public function should_send_notification_to_users_with_old_app_version()
     {
-        Mail::fake();
-
         $user = factory(User::class)->create();
-        $appServiceMock = Mockery::mock(UserAppService::class);
-        $appServiceMock->shouldReceive('getActiveUsersWithOldAppVersions')->andReturn(collect([$user]));
+        $this->mock->shouldReceive('getActiveUsersWithOldAppVersions')->andReturn(collect([$user]));
 
-        $this->expectsNotification($user, UpdateAppNotification::class);
+        $this->command->handle();
 
-        $command = new NotifyUsersWithOldApp($appServiceMock);
-        $command->handle();
+        Notification::assertSentTo($user, UpdateAppNotification::class);
+    }
+
+    /** @test */
+    public function should_not_send_notification_to_users_that_were_notified_recently()
+    {
+        $userThatWasAlreadyNotified = factory(User::class)->create();
+        $userThatWasAlreadyNotified->notifications()->save(new DatabaseNotification([
+            'id' => 'E5E33479-92C1-41E9-8505-A18CF854D8D1',
+            'type' => UpdateAppNotification::class,
+            'notifiable_type' => 'Caronae\Models\User',
+            'notifiable_id' => $userThatWasAlreadyNotified->id,
+            'data' => '{"userID":3,"app_platform":"iOS","app_version":"1.4.2"}',
+            'created_at' => new Carbon('yesterday'),
+        ]));
+
+        $userThatWasNotNotified = factory(User::class)->create();
+        $userThatWasNotNotified->notifications()->save(new DatabaseNotification([
+            'id' => 'E5E33479-92C1-41E9-8505-A18CF854D8D2',
+            'type' => 'OtherNotification',
+            'notifiable_type' => 'Caronae\Models\User',
+            'notifiable_id' => $userThatWasNotNotified->id,
+            'data' => '',
+            'created_at' => new Carbon('yesterday'),
+        ]));
+
+        $this->mock->shouldReceive('getActiveUsersWithOldAppVersions')->andReturn(collect([$userThatWasAlreadyNotified, $userThatWasNotNotified]));
+
+        $this->command->handle();
+
+        Notification::assertSentTo($userThatWasNotNotified, UpdateAppNotification::class);
+        Notification::assertNotSentTo($userThatWasAlreadyNotified, UpdateAppNotification::class);
     }
 }
