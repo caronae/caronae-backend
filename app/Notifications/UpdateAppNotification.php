2b5667a70fed698800622836a3345c6f3adf0db7
Mario Cecchi <macecchi@gmail.com>
2018-10-19 18:09:34 -0300

    Melhora a mensagem do email e adiciona link para a loja

--- app/Notifications/UpdateAppNotification.php
+++ app/Notifications/UpdateAppNotification.php
@@ -28,12 +28,21 @@ class UpdateAppNotification extends Notification implements ShouldQueue
 
     public function toMail(User $user)
     {
+        if ($user->app_platform == 'iOS') {
+            $store = 'App Store';
+            $storeURL = 'https://itunes.apple.com/us/app/caronae-ufrj/id1078790049';
+        } else {
+            $store = 'Google Play';
+            $storeURL = 'https://play.google.com/store/apps/details?id=br.ufrj.caronae';
+        }
+
         return (new MailMessage)
                     ->subject('Atualize seu app do Caronaê')
                     ->greeting("Oi, {$user->firstName}!")
-                    ->line('Parece que você está usando uma versão antiga do app do Caronaê.')
+                    ->line('Parece que você está usando uma **versão antiga** do app do Caronaê.')
                     ->line('Para continuarmos desenvolvendo melhorias no app, a equipe do Caronaê oferece suporte apenas às versões mais recentes lançadas.')
                     ->line('As nossas atualizações do app também sempre possuem correções de estabilidade e novas funcionalidades, então você só sai ganhando!')
-                    ->line('Atualize o app pela App Store ou Google Play para continuar usando o Caronaê.');
+                    ->line('**Atualize o app pela ' . $store . '** para continuar usando o Caronaê.')
+                    ->action('Caronaê na ' . $store, $storeURL);
     }
 }
